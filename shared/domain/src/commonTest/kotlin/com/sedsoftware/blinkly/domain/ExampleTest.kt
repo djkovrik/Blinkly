99c36c26cf891694b6f8ad081ea758b9e8f869d9
DJ_Kovrik <djkovrik@gmail.com>
2026-03-05 12:12:31 +0300

    Fix detekt issues

--- shared/domain/src/commonTest/kotlin/com/sedsoftware/blinkly/domain/ExampleTest.kt
+++ shared/domain/src/commonTest/kotlin/com/sedsoftware/blinkly/domain/ExampleTest.kt
@@ -3,6 +3,7 @@ package com.sedsoftware.blinkly.domain
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
+@Suppress("MemberNameEqualsClassName", "FunctionOnlyReturningConstant")
 class ExampleTest {
     private val example = Example()
 
