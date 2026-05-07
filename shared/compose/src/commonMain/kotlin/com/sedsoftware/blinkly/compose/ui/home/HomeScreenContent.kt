52f6a61651e5987ffc23fd979c26dd4225f77fe7
Sergey V. <djkovrik@gmail.com>
2026-05-07 21:30:07 +0300

    Bottom navbar widget (#9)
    * Customize bottom navigation bar
    
    * Update icons
    
    * Finalize composable
    
    * Fix github action

--- shared/compose/src/commonMain/kotlin/com/sedsoftware/blinkly/compose/ui/home/HomeScreenContent.kt
+++ shared/compose/src/commonMain/kotlin/com/sedsoftware/blinkly/compose/ui/home/HomeScreenContent.kt
@@ -2,33 +2,20 @@ package com.sedsoftware.blinkly.compose.ui.home
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import blinkly.shared.compose.generated.resources.Res
-import blinkly.shared.compose.generated.resources.tab_main
-import blinkly.shared.compose.generated.resources.tab_progress
-import blinkly.shared.compose.generated.resources.tab_reminders
-import blinkly.shared.compose.generated.resources.tab_trainings
 import com.arkivanov.decompose.extensions.compose.stack.Children
 import com.arkivanov.decompose.extensions.compose.stack.animation.fade
 import com.arkivanov.decompose.extensions.compose.stack.animation.stackAnimation
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import com.sedsoftware.blinkly.component.home.HomeScreenComponent
 import com.sedsoftware.blinkly.component.home.model.HomeScreenTab
+import com.sedsoftware.blinkly.compose.ui.home.bottomnavigation.BottomNavigationBar
 import com.sedsoftware.blinkly.compose.ui.home.tabs.MainTabContent
 import com.sedsoftware.blinkly.compose.ui.home.tabs.ProgressTabContent
 import com.sedsoftware.blinkly.compose.ui.home.tabs.RemindersTabContent
 import com.sedsoftware.blinkly.compose.ui.home.tabs.TrainingsTabContent
-import org.jetbrains.compose.resources.painterResource
 
 @Composable
 internal fun HomeScreenContent(
@@ -74,78 +61,5 @@ private fun ChildrenContent(
             is HomeScreenComponent.Child.ProgressTab -> ProgressTabContent(child.component)
             is HomeScreenComponent.Child.RemindersTab -> RemindersTabContent(child.component)
         }
-    }
-}
-
-@Composable
-private fun BottomNavigationBar(
-    activeTab: HomeScreenTab,
-    onTabClick: (HomeScreenTab) -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    NavigationBar(modifier = modifier.navigationBarsPadding()) {
-        NavigationBarItem(
-            label = { Text("Main") },
-            icon = {
-                Icon(
-                    painter = painterResource(resource = Res.drawable.tab_main),
-                    contentDescription = null,
-                    tint = MaterialTheme.colorScheme.primary,
-                    modifier = Modifier.size(size = 32.dp),
-                )
-            },
-            selected = activeTab == HomeScreenTab.MAIN,
-            onClick = { onTabClick.invoke(HomeScreenTab.MAIN) },
-            alwaysShowLabel = false,
-            modifier = modifier,
-        )
-
-        NavigationBarItem(
-            label = { Text("Trainings") },
-            icon = {
-                Icon(
-                    painter = painterResource(resource = Res.drawable.tab_trainings),
-                    contentDescription = null,
-                    tint = MaterialTheme.colorScheme.primary,
-                    modifier = Modifier.size(size = 32.dp),
-                )
-            },
-            selected = activeTab == HomeScreenTab.TRAININGS,
-            onClick = { onTabClick.invoke(HomeScreenTab.TRAININGS) },
-            alwaysShowLabel = false,
-            modifier = modifier,
-        )
-
-        NavigationBarItem(
-            label = { Text("Progress") },
-            icon = {
-                Icon(
-                    painter = painterResource(resource = Res.drawable.tab_progress),
-                    contentDescription = null,
-                    tint = MaterialTheme.colorScheme.primary,
-                    modifier = Modifier.size(size = 32.dp),
-                )
-            },
-            selected = activeTab == HomeScreenTab.PROGRESS,
-            onClick = { onTabClick.invoke(HomeScreenTab.PROGRESS) },
-            alwaysShowLabel = false,
-            modifier = modifier,
-        )
-
-        NavigationBarItem(
-            label = { Text("Reminders") },
-            icon = {
-                Icon(
-                    painter = painterResource(resource = Res.drawable.tab_reminders),
-                    contentDescription = null,
-                    tint = MaterialTheme.colorScheme.primary,
-                    modifier = Modifier.size(size = 32.dp),
-                )
-            },
-            selected = activeTab == HomeScreenTab.REMINDERS,
-            onClick = { onTabClick.invoke(HomeScreenTab.REMINDERS) },
-            alwaysShowLabel = false,
-            modifier = modifier,
-        )
     }
 }
